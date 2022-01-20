a45a3731cb5c56be8cdabe47243d15decc68b469
Weiliang Chen <montrealchina@yahoo.com>
2022-01-20 12:15:32 -0500

    added ButtonBases to landing page

--- src/views/LandingPage.js
+++ src/views/LandingPage.js
@@ -1,11 +1,27 @@
 import React from "react";
 import Button from '@mui/material/Button';
 import Stack from '@mui/material/Stack';
-import InputWithIcon from "../components/InputWithIcon"
 import FaceRetouchingNaturalIcon from '@mui/icons-material/FaceRetouchingNatural';
 import ManTwoToneIcon from '@mui/icons-material/ManTwoTone';
 import Box from '@mui/material/Box';
 import Collapse from '@mui/material/Collapse';
+import InputWithIcon from "../components/InputWithIcon";
+import ButtonBases from "../components/ButtonBases";
+
+const images = [
+  {
+    url: "/images/buttons/tree1.jpg",
+    href: "/tree/5ca4bbcea2dd94ee58162c36",
+    title: "View a Sample Tree",
+    width: "60%"
+  },
+  {
+    url: "/images/buttons/tree2.jpg",
+    href: "/id/0",
+    title: "Build a Tree",
+    width: "40%"
+  }
+];
 
 function Person(params) {
   let year = new Date(params.detail.birthdate).getFullYear();
@@ -46,12 +62,11 @@ function LandingPage(props) {
         <Button variant="outlined" color="primary" type="submit">Enter</Button>
       </Stack>
     </Box>
-    <div className="iconView">
     {data && data.map((item, x) => <Person key={x} detail={item} />)}
     <Collapse in={data===null}>
-      <li>No record found.  <a href='/id/0'>Create a new record?</a></li>
+    <h5>Seems nobody from this family has created any tree here. You are free to create a family tree for this name. So go ahead, give it a try, build a new Tree. Or view and play around a sample tree.</h5>
+    <ButtonBases Images={images} />
     </Collapse>
-    </div>
     <iframe src="" style={{border:'none'}} title="Iframe" name="iframe_a"></iframe>
     </Box>
     )
